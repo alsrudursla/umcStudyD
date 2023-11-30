d4f6f859e300b87096ff87b4901f5f1992a32438
alsrudursla <90559205+alsrudursla@users.noreply.github.com>
2023-11-30 19:04:45 +0900

    로그인 생략 부분 추가

--- src/main/java/UMC/studyD/controller/MemberController.java
+++ src/main/java/UMC/studyD/controller/MemberController.java
@@ -1,20 +1,28 @@
-package UMC.studyD.controller;
-
-import UMC.studyD.dto.SignInDto;
-import UMC.studyD.service.MemberService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-@Controller
-@RequiredArgsConstructor
-public class MemberController {
-    private final MemberService memberService;
-
-    // 회원 가입
-    @PostMapping("/user/signin")
-    public void register(@RequestBody SignInDto signInDto) {
-        memberService.signIn(signInDto);
-    }
-}
+//package UMC.studyD.controller;
+//
+//import UMC.studyD.dto.SignInDto;
+//import UMC.studyD.service.MemberService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class MemberController {
+//    private final MemberService memberService;
+//
+//    // 회원 가입
+//    @PostMapping("/user/signin")
+//    public void register(@RequestBody SignInDto signInDto) {
+//        memberService.signIn(signInDto);
+//    }
+//
+//    // 로그인
+//    @PostMapping("/user/login")
+//    public String login(@RequestBody LoginDto loginDto) {
+//        //customUserDetailService.loadUserByUsername(loginDto.getEmail());
+//        memberService.login(loginDto);
+//        return loginDto.getMessage();
+//    }
+//}
